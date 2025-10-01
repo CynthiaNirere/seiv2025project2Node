5714f83282c00829df2d4b94d5db9a50b0e37dcf
tessy.mugisha <mugishatessy@gmail.com>
2025-10-01 18:05:31 -0500

    index.js changes

--- app/models/index.js
+++ app/models/index.js
@@ -10,8 +10,9 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  }
+    idle: dbConfig.pool.idle
+  },
+  logging: false
 });
 
 const db = {};
